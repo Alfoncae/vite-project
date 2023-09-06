da2816f487b31f0f9ce8fb760e02af485d5e051a
Alfoncae <alfonsobran02@yahoo.com>
2023-09-06 16:36:53 -0700

    video change

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,6 +6,8 @@ export default function Card({video, darkMode}){
         <video 
             className={`mb-6 z-10 hover:cursor-pointer ${darkMode ? '' : 'border-slate-800 border-2'}`}
             controls
+            muted
+            controlsList="play pause nosound"
             width='650px'>    
 
             <source src={video} type='video/mp4' />
